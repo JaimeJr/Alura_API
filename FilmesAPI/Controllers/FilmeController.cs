11dbe16845cd32d37eb0028010c7c121ef629388
Jaime Junior <jaime-jr98@hotmail.com>
2022-09-03 14:58:22 -0300

    Retirado arquivos de exemplo. Adicionado model e controller filme e criado nova função para adição de filme

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+          filmes.Add(filme);
+        }
+    }
+}
