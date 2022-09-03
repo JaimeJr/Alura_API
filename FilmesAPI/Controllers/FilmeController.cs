405c95a6d284888bc2f861b45a3224d0f926cbd7
Jaime Junior <jaime-jr98@hotmail.com>
2022-09-03 19:39:41 -0300

    Alterado FilmeController para realizar a consulta e inclusão de itens no banco de dados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -9,32 +10,36 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-          filme.Id = id++;
-          filmes.Add(filme);
+          _context.Filmes.Add(filme);
+          _context.SaveChanges();
           return CreatedAtAction(nameof(RecurepaFilmesPorId), new {Id = filme.Id}, filme);
         }
 
         [HttpGet]
-        public IActionResult RecuperaFilmes()
+        public IEnumerable<Filme> RecuperaFilmes()
         {
-            return Ok(filmes);
+            return _context.Filmes;
         }
 
         [HttpGet("{id}")]
         public IActionResult RecurepaFilmesPorId(int id)
         {
-           Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
-            if (filme != null)
-            {
-                return Ok(filme);
-            }
-            return NotFound();
+           Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+           if (filme != null)
+           {
+             return Ok(filme);
+           }
+           return NotFound();
         }
     }
 }
