a8ad1495fba6327041610f67eb2af09ceab393ee
Jaime Junior <jaime-jr98@hotmail.com>
2022-09-03 15:32:00 -0300

    Adicionado função para retornar os filmes e adicionado campo ID à classe Filme

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -10,11 +10,19 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+          filme.Id = id++;
           filmes.Add(filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
         }
     }
 }
